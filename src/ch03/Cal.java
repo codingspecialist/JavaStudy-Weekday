82565af3fbed05ce007b53eb499fc6f6095a67b2
ssar <ssar@nate.com>
2020-03-20 12:51:53 +0900

    자바 2강 메서드2 v2

---
+++ src/ch03/Cal.java
@@ -0,0 +1,27 @@
+package ch03;
+
+public class Cal {
+	static int add(int n1, int n2) {
+		int result = n1 + n2;
+		// System.out.println("result : "+result);
+		return result; // return 가장 마지막에!!
+	}
+
+	// minus (매개변수는 int형 두개) (return은 int형)
+	static int minus(int n1, int n2) {
+		int result = n1 - n2;
+		return result;
+	}
+
+	// multi
+	static int multi(int n1, int n2) {
+		int result = n1 * n2;
+		return result;
+	}
+
+	// divide
+	static int divide(int n1, int n2) {
+		int result = n1 / n2;
+		return result;
+	}
+}
