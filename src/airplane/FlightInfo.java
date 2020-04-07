60122d31cc1e777ed08d743457be92e4678d8dca
ssar <ssar@nate.com>
2020-04-07 16:18:36 +0900

    자바 오브젝트 원형 변경

--- src/airplane/FlightInfo.java
+++ src/airplane/FlightInfo.java
@@ -1,27 +1,47 @@
 package airplane;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 
+import lombok.Data;
+
+@Data
+class Header{
+	private String resultCode;
+	private String resultMsg;
+}
+
+@Data
+class Item {
+	private String airlineNm;
+	private String arrAirportNm;
+	private long arrPlandTime;
+	private String depAirportNm;
+	private long depPlandTime;
+	private int economyCharge;
+	private int prestigeCharge;
+	private String vihicleId;
+}
+
+@Data
+class Items {
+	private ArrayList<Item> item;
+}
+
+@Data
+class Body{
+	private Items items;
+	private int numOfRows;
+	private int pageNo;
+	private int totalCount;
+}
+
+@Data
+class Response{
+	private Header header;
+	private Body body;
+}
+
+@Data
 public class FlightInfo {
-
-private Response response;
-private Map<String, Object> additionalProperties = new HashMap<String, Object>();
-
-public Response getResponse() {
-return response;
-}
-
-public void setResponse(Response response) {
-this.response = response;
-}
-
-public Map<String, Object> getAdditionalProperties() {
-return this.additionalProperties;
-}
-
-public void setAdditionalProperty(String name, Object value) {
-this.additionalProperties.put(name, value);
-}
-
+	private Response response;
 }
